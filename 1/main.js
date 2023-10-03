eef5f15de06bf19e0dba8aa8b70736a857e91b11
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-10-03 18:23:31 +0300

    create new class, place in a row, change novers

--- 1/main.js
+++ 1/main.js
@@ -1,6 +1,46 @@
-const fetchData = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0').then
-    (response => response.json()).then(data => console.log(data))
-}
+const content = document.querySelector(".pokedex");
+let pokeData = [1, 2, 3];
 
-fetchData()
+const fetchData = async () => {
+await
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=121&offset=0")
+    .then((response) => response.json())
+    .then((data) => { 
+        const fetches = data.results.map(item => {
+            return fetch(item.url).then(res => res.json())
+            .then(data => {
+                return {
+                    id: data.id,
+                    name: data.name,
+                    img: data.sprites.other['official-artwork'].front_default,
+                    types: data.types,
+                };
+            });
+        });
+        Promise.all(fetches).then(res => {
+            pokeData = res;
+            pokeCards();
+            // pokeData = data.results;
+            console.log(pokeData)
+        });
+    });
+};
+
+const pokeCards = () => {
+
+  const cards = pokeData.map(pokemon => {
+      return `
+      <div class="box">
+      <img class="img_pokemon" src="${pokemon.img}" />
+      <div class="number">${pokemon.id}</div>
+      <h2>${pokemon.name}</h2><div class="types">
+      <p>Water</p>
+      <p>Poison</p>
+      </div>
+      </div>`;
+  }).join('')
+  
+  content.innerHTML = cards;
+};
+
+fetchData();
