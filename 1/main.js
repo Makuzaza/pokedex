1b6c90b771390d96f811b696a3ac49b37f009f72
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-10-11 19:48:14 +0300

    add generations, add push buttons, add counts, style css

--- 1/main.js
+++ 1/main.js
@@ -70,46 +70,14 @@ generationButtons.forEach(button => {
     button.addEventListener('click', () => pushButtons(button));
 });
 
-const pushButtons = async (button) => {
-    const selectedGeneration = button.textContent;
-    const generationNumber = selectedGeneration.replace('Gen', '').trim(); 
-    const generationAnswer = await fetch(`https://pokeapi.co/api/v2/generation/${generationNumber}/`);
-    const generationData = await generationAnswer.json();
-    const pokemonNumber = generationData.pokemon_species.length;
-    const generationCount = document.getElementById('generation_count');
-    generationCount.textContent = `Number of Pokemons in ${selectedGeneration}: ${pokemonNumber}`;
-    const pokemonList = generationData.pokemon_species;
-    const fetches = pokemonList.map((species) => {
-        return fetch(species.url)
-        .then(res => res.json())
-        .then(data => {
-            return {
-                id: data.id,
-                name: data.name,
-                img: data.sprites.other['official-artwork'].front_default,
-                types: data.types,
-                height: data.height * 10,
-                weight: data.weight / 10
-            };
-        });
-    });
-    // const pokeData = await Promise.all(fetches);
-    // pokeCards(pokeData);
-    // console.log(pokeData)
-    Promise.all(fetches).then((res) => {
-        pokeData = res;
-        pokeCards(pokeData);
-        console.log(res); 
-    });
-}
-
 // const pushButtons = async (button) => {
 //     const selectedGeneration = button.textContent;
-//     const generationAnswer = await fetch(`https://pokeapi.co/api/v2/generation/`);
+//     const generationNumber = selectedGeneration.replace('Gen', '').trim(); 
+//     const generationAnswer = await fetch(`https://pokeapi.co/api/v2/generation/${generationNumber}/`);
 //     const generationData = await generationAnswer.json();
 //     const pokemonNumber = generationData.pokemon_species.length;
-//     const generationNumber = document.getElementById('generation_count');
-//     generationNumber.textContent = `Number of Pokémon in ${selectedGeneration}: ${pokemonNumber}`;
+//     const generationCount = document.getElementById('generation_count');
+//     generationCount.textContent = `Number of Pokemons in ${selectedGeneration}: ${pokemonNumber}`;
 //     const pokemonList = generationData.pokemon_species;
 //     const fetches = pokemonList.map((species) => {
 //         return fetch(species.url)
@@ -125,9 +93,90 @@ const pushButtons = async (button) => {
 //             };
 //         });
 //     });
-//     const pokeData = await Promise.all(fetches);
-//     pokeCards(pokeData);
-//     console.log(pokeData)
+//     // const pokeData = await Promise.all(fetches);
+//     // pokeCards(pokeData);
+//     // console.log(pokeData)
+//     Promise.all(fetches).then((res) => {
+//         pokeData = res;
+//         pokeCards(pokeData);
+//         console.log(res); 
+//     });
 // }
+// fetchData();
 
-fetchData();
+const pushButtons = async (button) => {
+    const selectedGeneration = button.textContent;
+    let offset, limit;
+
+    switch (selectedGeneration) {
+        case 'GenI':
+            offset = 0;
+            limit = 151;
+            break;
+        case 'GenII':
+            offset = 151;
+            limit = 100;
+            break;
+        case 'GenIII':
+            offset = 251;
+            limit = 135;
+            break;
+        case 'GenIV':
+            offset = 386;
+            limit = 107;
+            break;
+        case 'GenV':
+            offset = 493;
+            limit = 156;
+            break;
+        case 'GenVI':
+            offset = 649;
+            limit = 72;
+            break;
+        case 'GenVII':
+            offset = 721;
+            limit = 88;
+            break;
+        case 'GenVIII':
+            offset = 809;
+            limit = 96;
+            break;
+        case 'GenIX':
+            offset = 905;
+            limit = 112;
+            break;
+        default:
+            offset = 0;
+            limit = 0;
+            break;
+    }
+
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const fetches = data.results.map((species) => {
+        return fetch(species.url)
+            .then(res => res.json())
+            .then(data => {
+                return {
+                    id: data.id,
+                    name: data.name,
+                    img: data.sprites.other['official-artwork'].front_default,
+                    types: data.types,
+                    height: data.height * 10,
+                    weight: data.weight / 10
+                };
+            });
+    });
+
+    Promise.all(fetches).then((res) => {
+        pokeData = res;
+        pokeCards(pokeData);
+        const generationCount = document.getElementById('generation_count');
+        generationCount.textContent = `Number of Pokemons in Generation ${selectedGeneration.replace('Gen', '')}: ${res.length}`;
+        console.log(res);
+    });
+}
+
